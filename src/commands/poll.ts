7af29f9ab386e887c87187c001deec3162e9e0d0
Sarah Ohlin <sarahohlin1@gmail.com>
2020-05-30 18:55:00 -0400

    docs: better comment messy poll code

--- src/commands/poll.ts
+++ src/commands/poll.ts
@@ -6,20 +6,20 @@ import parse from 'parse-duration';
 async function f(msg: Discord.Message, args: string[]) {
 	await msg.delete();
 	
-	if (args.length < 5) {
+	//* validate argument input minimum length
+	if (args.length < 5 || args.length % 2 != 1) {
 		return msg.channel.send(error(
 			'Invalid format.',
 			`\`${process.env.PREFIX}poll ${fmt}\``
 		));
 	}
 	
-	let guild = msg.guild;
-	
 	let q = args[1];
 	let dur = parse(args[2], 'ms');
 	let opts: { emote: string | Discord.Emoji, opt: string }[] = [];
-	// parse all opts
+	//* parse options given into the `opts` array
 	for (let i = 3; i < args.length; i += 2) {
+		//* hacky code to test for different types of emotes
 		let emoteIdentifier: any = args[i + 1];
 		let re = /:(\w+):(\d+)/;
 		if (re.test(emoteIdentifier)) {
@@ -34,7 +34,7 @@ async function f(msg: Discord.Message, args: string[]) {
 		});
 	}
 
-	// construct the message embed
+	//* construct the message embed to vote on
 	let embed = new Discord.MessageEmbed()
 		.setTitle(q)
 		.setTimestamp(new Date())
@@ -49,10 +49,10 @@ async function f(msg: Discord.Message, args: string[]) {
 	});
 	embed.setDescription(optDisplays.join('\n'));
 
-	/// send the message 
+	//* send the embed 
 	let sentMessage: Discord.Message = await msg.channel.send(embed);
 	
-	/// create reaction listeners for the message
+	//* start listening to reactions!
 	const filter = (r: Discord.MessageReaction) => {
 		return !opts.find((v) => v.emote === r.emoji.identifier);
 	};
@@ -60,22 +60,23 @@ async function f(msg: Discord.Message, args: string[]) {
 		time: dur
 	})
 	.then((v) => {
-		// count total reactions
+		//* count total reactions
 		let total: number = 0;
 		v.forEach(r => total += r.count);
 		total -= v.size;
 
-		let results: string[] = []; 
-		let max: Discord.MessageReaction[] = [];
+		let results: string[] = []; // string for formatting the output
+		let max: Discord.MessageReaction[] = []; // the highest voted emoji
 
 		results.push(`Total votes: ${total}`);
 		v.forEach(r => {
-
+			//* validate that the emote is one of the options provided
 			let cOpt = opts.find(o => o.emote.toString() === r.emoji.toString())?.opt;
 			if (!cOpt) {
 				return;
 			}
 
+			//* hacky-ish maximum emote computation
 			if (!max || max.length == 0) {
 				max = [];
 				max.push(r)
@@ -86,23 +87,27 @@ async function f(msg: Discord.Message, args: string[]) {
 				max.push(r);
 			}
 			
+			//* calculating the percent and formatting for the output
 			let percent: number = total != 0 ? (r.count - 1) / total : 0;
 			results.push(`• ${r.emoji.toString()} ${cOpt} -- ${(percent * 100).toFixed(2)}%`);
 		});
+		//* format the winning results
 		let reducedResults: string[] = [];
 		max.forEach((r: Discord.MessageReaction) => {
 			let cOpt = opts.find(o => o.emote.toString() === r.emoji.toString())?.opt;
 			reducedResults.push(`• ${r.emoji.toString()} ${cOpt}\n`);
 		});
+		//* push final winner to results
 		results.push(`Winner(s): ${reducedResults.length != 0 ? '\n' + reducedResults : 'N/A'}`);
 
+		//* output results as message
 		return msg.channel.send(success(
 			`Poll resolved: ${q}`,
 			results.join('\n')
 		));
 	});
 
-	/// react with the options
+	//* react with the options
 	await asyncForEach(opts, async (opt) => {
 		await sentMessage.react(opt.emote);	
 	});
